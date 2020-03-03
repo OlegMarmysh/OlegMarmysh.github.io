9655b9ef943e6e4c1e79fe7857a60a06a3f74a42
OlegMarmysh <56603835+OlegMarmysh@users.noreply.github.com>
2020-03-03 23:57:04 +0300

    add adaptive burger menu and block of projects displayed from array object

--- internship/burgerMenu.js
+++ internship/burgerMenu.js
@@ -1,45 +1,32 @@
 const burgerMenu = document.querySelector('.menu__burger');
 const navMenu = document.querySelector('.menu');
-const body = document.querySelector('body');
-const menuFirst = document.querySelector('.menu__first>div');
-const menuSecond = document.querySelector('.menu__second>div');
-const menuThird = document.querySelector('.menu__third>div');
-const menuForth = document.querySelector('.menu__forth>div');
-const menuFirstSpoiler = document.querySelector('.menu__first>ul');
-const menuSecondSpoiler = document.querySelector('.menu__second>ul');
-const menuThirdSpoiler = document.querySelector('.menu__third>ul');
-const menuForthSpoiler = document.querySelector('.menu__forth>ul');
+const navMenuItems = navMenu.querySelectorAll('.menu__container > .menu__item');
+
 
 
 const createBurgerMenu = function () {
     burgerMenu.classList.toggle('active');
     navMenu.classList.toggle('active');
-    body.classList.toggle('lock');
+    document.body.classList.toggle('lock');
 };
 
-const createFirstSpoilerMenu =  () => {
-    menuFirstSpoiler.classList.toggle('active')
-    menuFirst.classList.toggle('flipped')
-};
-const createSecondSpoilerMenu =  () => {
-    menuSecondSpoiler.classList.toggle('active')
-    menuSecond.classList.toggle('flipped')
-};
-const createThirdSpoilerMenu =  () => {
-    menuThirdSpoiler.classList.toggle('active')
-    menuThird.classList.toggle('flipped')
-};
-const createForthSpoilerMenu =  () => {
-    menuForthSpoiler.classList.toggle('active')
-    menuForth.classList.toggle('flipped')
+handleToggleSubmenu = item => {
+    item.querySelector('.submenu-item').classList.toggle('active');
+    navMenuItems.forEach( menuItem =>{
+        if (menuItem !== item){
+            menuItem.querySelector('.submenu-item').classList.remove('active')
+        }
+    })
 };
 
 
 burgerMenu.addEventListener('click', createBurgerMenu);
-menuFirst.addEventListener('click', createFirstSpoilerMenu);
-menuSecond.addEventListener('click', createSecondSpoilerMenu);
-menuThird.addEventListener('click', createThirdSpoilerMenu);
-menuForth.addEventListener('click', createForthSpoilerMenu);
+
+navMenuItems.forEach( li => {
+   const toggleSubmenu = () => handleToggleSubmenu (li);
+    li.addEventListener("click", toggleSubmenu)
+} );
+
 
 
 
---
+++ internship/index.js
@@ -0,0 +1,66 @@
+const spinProjects = [
+    {
+        img: './image/spring-boot.svg',
+        title: 'Spring boot',
+        body: 'Takes an opinionated view of building Spring applications and gets you up and running as quickly as possible'
+    },
+    {
+        img: './image/spring-framework.svg',
+        title: 'Spring framework',
+        body: 'Provides core support for dependency injection, transaction management, web apps, data access, messaging and more.'
+    },
+    {
+        img: './image/spring-data.svg',
+        title: 'Spring data',
+        body: 'Provides a consistent approach to data access – relational, non-relational, map-reduce, and beyond.'
+    },
+    {
+        img: './image/spring-cloud.svg',
+        title: 'Spring cloud',
+        body: 'Provides a set of tools for common patterns in distributed systems. Useful for building and deploying microservices.'
+    }
+];
+const spinAtticProjects = [
+    {
+        img: './image/logo-io-platform.png',
+        title: 'Spring IO Platform',
+        body: 'Provides a cohesive, versioned platform for building modern applications. It is a modular, enterprise-grade distribution that delivers a curated set of dependencies.'
+    },
+    {
+        img: './image/spring-ed.svg',
+        title: 'Spring Flex',
+        body: 'A project to make it easier to build Spring-powered Rich Internet Applications using Adobe Flex as the front-end client.'
+    },
+    {
+        img: './image/spring-ed.svg',
+        title: 'Spring integration Groovy DSL',
+        body: 'A Groovy based DSL for developing Spring Integration applications.'
+    },
+    {
+        img: './image/spring-ed.svg',
+        title: 'Spring integration Scala DSL',
+        body: 'A Scala based DSL for developing Spring Integration applications.'
+    }
+]
+
+const spinProjectEl = document.querySelector('.spin-projects');
+spinProjects.map(el => spinProjectEl.insertAdjacentHTML('afterbegin', `  <div>
+                <div>
+                    <img src=${el.img} alt="a11y">
+                </div>
+                <div>
+                    <h3>${el.title}</h3>
+                    <p>${el.body}</p>
+                </div>
+            </div>`));
+const spinAtticProjectEl = document.querySelector('.spin-attic-projects>div');
+spinAtticProjects.map(el => spinAtticProjectEl.insertAdjacentHTML('afterbegin', `  <div>
+                <div>
+                    <img src=${el.img} alt="a11y">
+                </div>
+                <div>
+                    <h3>${el.title}</h3>
+                    <p>${el.body}</p>
+                </div>
+            </div>`));
+
