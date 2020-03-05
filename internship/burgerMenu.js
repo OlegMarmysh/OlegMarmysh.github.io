fe5f82658f58828dab231d35a41d961ed69ec6b0
OlegMarmysh <56603835+OlegMarmysh@users.noreply.github.com>
2020-03-06 01:03:45 +0300

    change img alt and add search form by title of projects

--- internship/burgerMenu.js
+++ internship/burgerMenu.js
@@ -1,31 +1,30 @@
-const burgerMenu = document.querySelector('.menu__burger');
-const navMenu = document.querySelector('.menu');
-const navMenuItems = navMenu.querySelectorAll('.menu__container > .menu__item');
-
+const burgerMenu = document.querySelector(".menu__burger");
+const navMenu = document.querySelector(".menu");
+const navMenuItems = navMenu.querySelectorAll(".menu__container > .menu__item");
 
 
 const createBurgerMenu = function () {
-    burgerMenu.classList.toggle('active');
-    navMenu.classList.toggle('active');
-    document.body.classList.toggle('lock');
+    burgerMenu.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    document.body.classList.toggle("lock");
 };
 
 handleToggleSubmenu = item => {
-    item.querySelector('.submenu-item').classList.toggle('active');
-    navMenuItems.forEach( menuItem =>{
-        if (menuItem !== item){
-            menuItem.querySelector('.submenu-item').classList.remove('active')
+    item.querySelector(".submenu-item").classList.toggle("active");
+    navMenuItems.forEach(menuItem => {
+        if (menuItem !== item) {
+            menuItem.querySelector(".submenu-item").classList.remove("active")
         }
     })
 };
 
 
-burgerMenu.addEventListener('click', createBurgerMenu);
+burgerMenu.addEventListener("click", createBurgerMenu);
 
-navMenuItems.forEach( li => {
-   const toggleSubmenu = () => handleToggleSubmenu (li);
+navMenuItems.forEach(li => {
+    const toggleSubmenu = () => handleToggleSubmenu(li);
     li.addEventListener("click", toggleSubmenu)
-} );
+});
 
 
 
--- internship/index.js
+++ internship/index.js
@@ -1,50 +1,80 @@
 const spinProjects = [
     {
-        img: './image/spring-boot.svg',
-        title: 'Spring boot',
-        body: 'Takes an opinionated view of building Spring applications and gets you up and running as quickly as possible'
+        img: "./image/spring-boot.svg",
+        title: "Spring boot",
+        body: "Takes an opinionated view of building Spring applications and gets you up and running as quickly as possible"
     },
     {
-        img: './image/spring-framework.svg',
-        title: 'Spring framework',
-        body: 'Provides core support for dependency injection, transaction management, web apps, data access, messaging and more.'
+        img: "./image/spring-framework.svg",
+        title: "Spring framework",
+        body: "Provides core support for dependency injection, transaction management, web apps, data access, messaging and more."
     },
     {
-        img: './image/spring-data.svg',
-        title: 'Spring data',
-        body: 'Provides a consistent approach to data access – relational, non-relational, map-reduce, and beyond.'
+        img: "./image/spring-data.svg",
+        title: "Spring data",
+        body: "Provides a consistent approach to data access – relational, non-relational, map-reduce, and beyond."
     },
     {
-        img: './image/spring-cloud.svg',
-        title: 'Spring cloud',
-        body: 'Provides a set of tools for common patterns in distributed systems. Useful for building and deploying microservices.'
+        img: "./image/spring-cloud.svg",
+        title: "Spring cloud",
+        body: "Provides a set of tools for common patterns in distributed systems. Useful for building and deploying microservices."
     }
 ];
 const spinAtticProjects = [
     {
-        img: './image/logo-io-platform.png',
-        title: 'Spring IO Platform',
-        body: 'Provides a cohesive, versioned platform for building modern applications. It is a modular, enterprise-grade distribution that delivers a curated set of dependencies.'
+        img: "./image/logo-io-platform.png",
+        title: "Spring IO Platform",
+        body: "Provides a cohesive, versioned platform for building modern applications. It is a modular, enterprise-grade distribution that delivers a curated set of dependencies."
     },
     {
-        img: './image/spring-ed.svg',
-        title: 'Spring Flex',
-        body: 'A project to make it easier to build Spring-powered Rich Internet Applications using Adobe Flex as the front-end client.'
+        img: "./image/spring-ed.svg",
+        title: "Spring Flex",
+        body: "A project to make it easier to build Spring-powered Rich Internet Applications using Adobe Flex as the front-end client."
     },
     {
-        img: './image/spring-ed.svg',
-        title: 'Spring integration Groovy DSL',
-        body: 'A Groovy based DSL for developing Spring Integration applications.'
+        img: "./image/spring-ed.svg",
+        title: "Spring integration Groovy DSL",
+        body: "A Groovy based DSL for developing Spring Integration applications."
     },
     {
-        img: './image/spring-ed.svg',
-        title: 'Spring integration Scala DSL',
-        body: 'A Scala based DSL for developing Spring Integration applications.'
+        img: "./image/spring-ed.svg",
+        title: "Spring integration Scala DSL",
+        body: "A Scala based DSL for developing Spring Integration applications."
     }
-]
+];
 
-const spinProjectEl = document.querySelector('.spin-projects');
-spinProjects.map(el => spinProjectEl.insertAdjacentHTML('afterbegin', `  <div>
+
+const spinProjectEl = document.querySelector(".spin-projects");
+spinProjects.map(el => spinProjectEl.insertAdjacentHTML("afterbegin", `  <div>
+                <div>
+                    <img src=${el.img} alt="project_logo">
+                </div>
+                <div>
+                    <h3>${el.title}</h3>
+                    <p>${el.body}</p>
+                </div>
+            </div>`));
+const spinAtticProjectEl = document.querySelector(".spin-attic-projects>div");
+spinAtticProjects.map(el => spinAtticProjectEl.insertAdjacentHTML("afterbegin", `  <div>
+                <div>
+                    <img src=${el.img} alt="attic_project_logo">
+                </div>
+                <div>
+                    <h3>${el.title}</h3>
+                    <p>${el.body}</p>
+                </div>
+            </div>`));
+
+
+const input = document.querySelector(".place_for_search");
+const searchButton = document.querySelector(".search-block button");
+
+
+const findElement = (e) => {
+    e.preventDefault();
+    const elem = spinProjects.filter(el => el.title === input.value);
+    if (elem.length) {
+        elem.map((el => spinProjectEl.innerHTML = `<div>
                 <div>
                     <img src=${el.img} alt="a11y">
                 </div>
@@ -53,14 +83,10 @@ spinProjects.map(el => spinProjectEl.insertAdjacentHTML('afterbegin', `  <div>
                     <p>${el.body}</p>
                 </div>
             </div>`));
-const spinAtticProjectEl = document.querySelector('.spin-attic-projects>div');
-spinAtticProjects.map(el => spinAtticProjectEl.insertAdjacentHTML('afterbegin', `  <div>
-                <div>
-                    <img src=${el.img} alt="a11y">
-                </div>
-                <div>
-                    <h3>${el.title}</h3>
-                    <p>${el.body}</p>
-                </div>
-            </div>`));
+    } else {
+        spinProjectEl.innerHTML = "No results"
+    }
+};
 
+
+searchButton.addEventListener("click", findElement);
